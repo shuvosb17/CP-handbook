26fae4e592d1e0f0cba15472c7f80d469c06a1e2
Shuvo <129549247+shuvosb17@users.noreply.github.com>
2025-10-30 20:49:16 +0600

    Create A. Array Coloring.cpp

---
+++ TLE Eliminators/CF 800 Rated/A. Array Coloring.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> arr(n);
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        for(auto it:arr){
+            sum+=it;
+        }
+        if(sum%2==0){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+ 
+    return 0;
+}
